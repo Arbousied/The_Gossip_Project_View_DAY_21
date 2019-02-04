a9461426bbb808a91acc1a4049dac9cb362796be
Clodiap <clodiapp@gmail.com>
2019-02-04 15:58:03 +0100

    affichage gossip sur page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'gossips/:id', to: 'gossips#show'
   get 'welcome/:user_entry', to: 'welcome#show'
   get 'team', to: 'static_pages#team'
   get 'contact', to: 'static_pages#contact'
