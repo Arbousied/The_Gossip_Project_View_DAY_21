6b5bf2a30d36911577efb4102fa6e8bd85d1af21
Clodiap <clodiapp@gmail.com>
2019-02-04 14:55:43 +0100

    dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/:user_entry', to: 'welcome#show'
   get 'team', to: 'static_pages#team'
   get 'contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
